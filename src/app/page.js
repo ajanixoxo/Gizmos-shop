363a755cdd2c461e8545f6af58de0f24bdb67383
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-08-18 08:58:45 +0100

    Fix font issue

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,6 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Image from "next/image";
 import { useEffect } from "react";
-const MDBCSS = dynamic(() => import('mdb-react-ui-kit/dist/css/mdb.min.css'), { ssr: false });
-const FontAwesomeCSS = dynamic(() => import("@fortawesome/fontawesome-free/css/all.min.css"), { ssr: false });
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
