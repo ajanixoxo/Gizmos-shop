aba629ab9ec6da48be305ab9157f1cf5343af2a7
Adeoluwa Gbenro <joelayomide35@gmail.com>
2024-08-29 17:32:44 +0100

    Cooked

--- src/app/components/ESI/carousel.js
+++ src/app/components/ESI/carousel.js
@@ -52,6 +52,94 @@ function Carousel({text}) {
       
       </MDBCarouselItem>
      
+      <MDBCarouselItem itemId={2}  >
+        <div className="flex flex-col md:flex-row justify-center md:justify-center items-center gap-10  car-left w-full" >
+        <div className="c-left mt-2 p-0 md:p-4 md:mt-0 text-left md:w-40 md:ml-2 ">
+          <h1 className="main-text font-semibold w-max md:w-20 ">Limited Weekly Discount</h1>
+          <p className="text-gray-500 text-[12px] font-semibold md:text-sm md:w-max text-center">Enjoy the sales why you can&apos;t</p>
+        </div>
+        <div className="c-right flex 
+        flex-col md:flex-row 
+        md:w-5/6
+        justify-start items-center gap-4 md:gap-0 ">
+        <Image src={Wrist}  width={300}
+      height={300} className='d-block c-img ' alt='...' />
+      <div className="details flex flex-col justify-center items-center">
+        <div className="count-down flex gap-2 mb-4">
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Days</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">03</h1>
+          </div>
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Hours</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">14</h1>
+          </div>
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Minutes</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">25</h1>
+          </div>
+         
+         <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Days</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">05</h1>
+          </div>
+       
+         
+        </div>
+        <p className="watch-info text-[15px]  md:text-xs font-bold w-8/12 text-center">Gizmo Vivobook 15 Thin and Light Laptop 15.6" FHD Display</p>
+       
+        <h1 className="price text-3xl">$12.050 <sup className="text-lg text-gray-400 line-through">$1500</sup></h1>
+      </div>
+        </div>
+        </div>
+    
+      
+      </MDBCarouselItem>
+     
+      <MDBCarouselItem itemId={3}  >
+        <div className="flex flex-col md:flex-row justify-center md:justify-center items-center gap-10  car-left w-full" >
+        <div className="c-left mt-2 p-0 md:p-4 md:mt-0 text-left md:w-40 md:ml-2 ">
+          <h1 className="main-text font-semibold w-max md:w-20 ">Limited Weekly Discount</h1>
+          <p className="text-gray-500 text-[12px] font-semibold md:text-sm md:w-max text-center">Enjoy the sales why you can&apos;t</p>
+        </div>
+        <div className="c-right flex 
+        flex-col md:flex-row 
+        md:w-5/6
+        justify-start items-center gap-4 md:gap-0 ">
+        <Image src={Wrist}  width={300}
+      height={300} className='d-block c-img ' alt='...' />
+      <div className="details flex flex-col justify-center items-center">
+        <div className="count-down flex gap-2 mb-4">
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Days</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">03</h1>
+          </div>
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Hours</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">14</h1>
+          </div>
+          <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Minutes</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">25</h1>
+          </div>
+         
+         <div className="count-box text-center border-2 border-gray-400 rounded-lg bg-white p-2 w-16 ">
+            <p className="text-gray-400 uppercase text-[8px] m-0 text-left font-bold">Days</p>
+            <h1 className="m-0 text-right text-3xl font-semibold">05</h1>
+          </div>
+       
+         
+        </div>
+        <p className="watch-info text-[15px]  md:text-xs font-bold w-8/12 text-center">Gizmo Vivobook 15 Thin and Light Laptop 15.6" FHD Display</p>
+       
+        <h1 className="price text-3xl">$12.050 <sup className="text-lg text-gray-400 line-through">$1500</sup></h1>
+      </div>
+        </div>
+        </div>
+    
+      
+      </MDBCarouselItem>
+     
      
     </MDBCarousel>
     </div>
--- src/app/components/ESI/carousel2.js
+++ src/app/components/ESI/carousel2.js
@@ -27,6 +27,44 @@ function Carousel2({ text }) {
 
           </div>
         </MDBCarouselItem>
+        <MDBCarouselItem itemId={2} >
+          <div className="flex justify-between m-2 md:m-0 md:justify-center md:gap-8 items-center c-container z-20" >
+            <div className=" w-1/2 md:w-[25%] z-10">
+              <div className="">
+
+                <h1 className="c2 text-[20px] md:text-[40px] text-black w-[200px] md:w-[400px]">Connecting easier than ever before</h1>
+                <p className="text-sm text-bold text-gray-500">ASUS CX70 2QF-62XPLl</p></div>
+              <div>
+                <p className="text-sm text-bold text-gray-500">STARTING AT </p>
+                <h5 className="text-2xl md:text-4xl text-black">$789</h5>
+              </div>
+            </div>
+
+            <Image src={Headphone} width={400}
+              height={400} className='m-0 w-[600px] md:w-[500px] overflow-hidden' alt='...' />
+
+
+          </div>
+        </MDBCarouselItem>
+        <MDBCarouselItem itemId={3} >
+          <div className="flex justify-between m-2 md:m-0 md:justify-center md:gap-8 items-center c-container z-20" >
+            <div className=" w-1/2 md:w-[25%] z-10">
+              <div className="">
+
+                <h1 className="c2 text-[20px] md:text-[40px] text-black w-[200px] md:w-[400px]">Connecting easier than ever before</h1>
+                <p className="text-sm text-bold text-gray-500">ASUS CX70 2QF-62XPLl</p></div>
+              <div>
+                <p className="text-sm text-bold text-gray-500">STARTING AT </p>
+                <h5 className="text-2xl md:text-4xl text-black">$789</h5>
+              </div>
+            </div>
+
+            <Image src={Headphone} width={400}
+              height={400} className='m-0 w-[600px] md:w-[500px] overflow-hidden' alt='...' />
+
+
+          </div>
+        </MDBCarouselItem>
      
 
       </MDBCarousel>
